46553563519cbf3621d5f629a8702cf640742220
Dardaul Hoque <rudro1152@gmail.com>
2024-12-01 19:45:26 +0600

    add smart split for wezterm and neovim

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,48 +1,23 @@
 local wezterm = require("wezterm")
+local scrollback_in_vim_keybinds = require("scrollback_in_vim")
+local pane_split_keybinds = require("pane_split")
 
-local io = require("io")
-local os = require("os")
-local act = wezterm.action
+local function appendTable(t1, t2)
+	for _, value in ipairs(t2) do
+		table.insert(t1, value)
+	end
+end
 
-wezterm.on("trigger-vim-with-scrollback", function(window, pane)
-	-- Retrieve the text from the pane
-	local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+local config = wezterm.config_builder()
 
-	-- Create a temporary file to pass to vim
-	local name = os.tmpname()
-	local f = io.open(name, "w+")
-	f:write(text)
-	f:flush()
-	f:close()
+config.color_scheme = "carbonfox"
+config.font = wezterm.font({ family = "Berkeley Mono" })
+config.font_size = 14
 
-	-- Open a new window running vim and tell it to open the file
-	window:perform_action(
-		act.SpawnCommandInNewWindow({
-			args = { "nvim", name },
-		}),
-		pane
-	)
+-- keybinds
+config.keys = {}
+config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 }
+appendTable(config.keys, scrollback_in_vim_keybinds)
+appendTable(config.keys, pane_split_keybinds)
 
-	-- Wait "enough" time for vim to read the file before we remove it.
-	-- The window creation and process spawn are asynchronous wrt. running
-	-- this script and are not awaitable, so we just pick a number.
-	--
-	-- Note: We don't strictly need to remove this file, but it is nice
-	-- to avoid cluttering up the temporary directory.
-	wezterm.sleep_ms(1000)
-	os.remove(name)
-end)
-
-return {
-	color_scheme = "carbonfox",
-
-	font = wezterm.font({ family = "Berkeley Mono" }),
-	font_size = 14,
-	keys = {
-		{
-			key = "v",
-			mods = "CTRL",
-			action = act.EmitEvent("trigger-vim-with-scrollback"),
-		},
-	},
-}
+return config

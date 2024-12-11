2af524d97a102ff828c28e7165cc6da14c73730c
Dardaul Hoque <rudro1152@gmail.com>
2024-12-11 15:23:44 +0600

    wezterm: add keybinds for closing panes and tabs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,14 +10,28 @@ end
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "carbonfox"
+config.color_scheme = "Default (dark) (terminal.sexy)"
 config.font = wezterm.font({ family = "Berkeley Mono" })
-config.font_size = 14
+config.font_size = 15
 
 -- keybinds
 config.keys = {}
 config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 }
 appendTable(config.keys, scrollback_in_vim_keybinds)
 appendTable(config.keys, pane_split_keybinds)
+appendTable(config.keys, {
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+})
+appendTable(config.keys, {
+	{
+		key = "q",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+})
 
 return config
